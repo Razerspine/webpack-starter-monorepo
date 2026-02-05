8b5cb26802580854c153ffa97d40a3e020f35d74
bagel <ponomarenko.pavel.web@gmail.com>
2026-02-05 20:07:34 +0200

    add @razerspine/webpack-core for template pug-scss-js

--- packages/templates/pug-scss-js/files/webpack.config.js
+++ packages/templates/pug-scss-js/files/webpack.config.js
@@ -1,5 +1,49 @@
-const createConfig = require('./webpack');
+const path = require("path");
+const {
+  createBaseConfig,
+  createDevConfig,
+  createProdConfig,
+} = require("@razerspine/webpack-core");
+const uiKit = require("@razerspine/pug-ui-kit");
 
+/**
+ * createBaseConfig:
+ *  - mode: development | production
+ *  - scripts: js | ts
+ *  - styles: scss | less
+ *  - templates: {
+ *  -    entry: 'src/views/pages'
+ *  - },
+ *  - resolve: {
+ *  -   alias: {...your_aliases}
+ *  - }
+ */
 module.exports = (env = {}, argv = {}) => {
-  return createConfig(env, argv);
+  const mode = argv?.mode || env?.mode || process.env.NODE_ENV || "development";
+
+  const baseConfig = createBaseConfig({
+    mode,
+    scripts: "js",
+    styles: "scss",
+    templates: {
+      entry: "src/views/pages/",
+    },
+    resolve: {
+      alias: {
+        "@views": path.resolve(process.cwd(), "src/views"),
+        "@styles": path.resolve(process.cwd(), "src/assets/styles"),
+        "@scripts": path.resolve(process.cwd(), "src/assets/scripts"),
+        "@fonts": path.resolve(process.cwd(), "src/assets/fonts"),
+        "@images": path.resolve(process.cwd(), "src/assets/images"),
+        "@icons": path.resolve(process.cwd(), "src/assets/icons"),
+        "pug-ui-kit": uiKit.paths.mixins,
+      },
+    },
+  });
+
+  if (mode === "development") {
+    return createDevConfig(baseConfig);
+  }
+
+  return createProdConfig(baseConfig);
 };
--- packages/templates/pug-scss-js/files/webpack/loaders/scripts.js
+++
@@ -1,9 +0,0 @@
-module.exports = () => {
-  return {
-    test: /\.m?js$/,
-    exclude: /node_modules/,
-    use: {
-      loader: 'babel-loader',
-    },
-  };
-};
